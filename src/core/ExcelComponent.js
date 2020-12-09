a5fb87feb43d48dbcd117a3b377768d5e6dcb4af
StarBurstm <panda.code.lab@gmail.com>
2020-12-09 13:06:39 +0300

    Lesson 30 start

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,11 @@
+import { DomListener } from "./DomListener";
+
+import {DomListener} from "@core/DomListener"
+
+export class ExcelComponent extends DomListener {
+
+    // Возвращает шаблон компонента
+    toHTML() {
+        return ''
+    }
+}
