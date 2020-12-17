01075d3b060834a5785e2c261fc196a122c49b17
StarBurstm <panda.code.lab@gmail.com>
2020-12-17 15:25:09 +0300

    GO

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,23 @@
 export class Excel {
-    constructor(selector, options) {
-        this.$el = document.querySelector(selector)
-        this.components = options.components || []
-    }
+  constructor(selector, options) {
+    this.$el = document.querySelector(selector)
+    this.components = options.components || []
+  }
+
+  getRoot() {
+    const $root = document.createElement('div')
+
+    this.components.forEach(Component => {
+      const component = new Component()
+      $root.insertAdjacentHTML('afterbegin', component.toHTML())
+    });
+
+    return $root
+  }
+
+  render() {
+    this.$el.append(this.getRoot())
+  }
 }
+
+
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,11 +1,8 @@
-import { DomListener } from "./DomListener";
-
-import {DomListener} from "@core/DomListener"
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
-
-    // Возвращает шаблон компонента
-    toHTML() {
-        return ''
-    }
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
 }
